54e0496a1537d1735614bf7b59255fa21223cbe9
kristina <krisbubenka@icloud.com>
2024-08-21 17:19:57 +0300

    comlpete task and test and fix bags

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) {
+        return [];
+    }
+    let newArr = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            newArr = newArr.concat(matrix[i]);
+        } else {
+            newArr = newArr.concat(matrix[i].reverse());
+        }
+    }
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+    return newArr;
 }
+
+let res = towelSort([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+]);
+console.log(res);
+
+module.exports = towelSort;
